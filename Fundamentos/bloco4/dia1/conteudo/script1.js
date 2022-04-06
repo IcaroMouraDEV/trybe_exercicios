9c86108337468db576bd9b9275407eb64bb97009
Icaro Joel <icaromoura.programador@gmail.com>
2022-04-06 15:42:56 -0300

    fazendo a primeira parte do conteudo

---
+++ Fundamentos/bloco4/dia1/conteudo/script1.js
@@ -0,0 +1,15 @@
+//Variaveis
+const myName = "Ícaro Joel Moura";
+const birthCity = "Petrolina-Pe";
+const birthYear = 2003;
+birthYear = 2030;
+birthCity = "Uauá-Ba";
+//Mostrando no console
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+/*
+    MOTIVO DE TER DADO ERRO
+
+    CONST SO PODE TER UMA ATRIBUIÇÃO :thumbsup:
+*/
