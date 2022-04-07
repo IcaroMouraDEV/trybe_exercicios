62aeaab7dcf3f040e338502b8a7f50d5ede3db8e
Icaro Joel <icaromoura.programador@gmail.com>
2022-04-07 18:45:20 -0300

    exercicio 2

--- Fundamentos/bloco4/dia2/exercicio/script.js
+++ Fundamentos/bloco4/dia2/exercicio/script.js
@@ -1,6 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercício 1
-console.log(numbers)
+console.log(numbers);
 
 // Exercício 2
+let somaArray = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    somaArray += numbers[i];
+}
+
+console.log(somaArray);
