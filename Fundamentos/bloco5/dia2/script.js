018ab760878cb68d044c078b3600165990bea123
Icaro Joel <icaromoura.programador@gmail.com>
2022-04-14 15:54:51 -0300

    fazendo o desafio 1 do exercicio

---
+++ Fundamentos/bloco5/dia2/script.js
@@ -0,0 +1,6 @@
+// Exercício 1
+let title = document.querySelector('body');
+let finalTitle = document.createElement('h1');
+finalTitle.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+
+title.appendChild(finalTitle);
