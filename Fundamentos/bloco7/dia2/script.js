5118f02b93e07e090b0c864c21b09b76e40437da
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-09 17:06:19 -0300

    exercício 1 completo

--- Fundamentos/bloco7/dia2/script.js
+++ Fundamentos/bloco7/dia2/script.js
@@ -1,12 +1,36 @@
+const verify = (value1, value2) => {
+  if (!value1 || !value2) {
+    throw new Error("Informe os dois valores para a soma ser realizada");
+  }
+};
+
+const verifyNan = (value) => {
+  const erro = isNaN(value);
+
+  if(erro === true) {
+    throw new Error("os valores não são numéricos");
+  }
+}
+
 function sum() {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  const result = parseInt(value1) + parseInt(value2);
-  document.getElementById('result').innerHTML = `Resultado: ${result}`;
-  document.getElementById('value1').value = '';
-  document.getElementById('value2').value = '';
+  try {
+    const value1 = document.getElementById("value1").value;
+    const value2 = document.getElementById("value2").value;
+    verify(value1, value2);
+    verifyNan(value1);
+    verifyNan(value2);
+    const result = parseInt(value1) + parseInt(value2);
+    document.getElementById("result").innerHTML = `Resultado: ${result}`; 
+  } catch (error) {
+    document.getElementById("result").innerHTML = `Erro: ${error.message}`;
+  }
+
+  finally {
+    document.getElementById("value1").value = "";
+    document.getElementById("value2").value = "";
+  }
 }
 window.onload = () => {
-  const button = document.getElementById('button');
-  button.addEventListener('click', sum);
-}
+  const button = document.getElementById("button");
+  button.addEventListener("click", sum);
+};
