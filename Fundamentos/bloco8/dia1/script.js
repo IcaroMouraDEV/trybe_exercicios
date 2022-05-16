9272206c4b1ee3acfa7845548b714278d118ce82
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-16 15:39:04 -0300

    exercício bónus parte 1

--- Fundamentos/bloco8/dia1/script.js
+++ Fundamentos/bloco8/dia1/script.js
@@ -46,3 +46,33 @@ const corrigir = (student, correctAnswer) => {
 }
 
 console.log(corrigir(estudante, gabarito));
+
+// Exercício bónus
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDmg = (dragon) => {
+  minDmg = 15;
+  return Math.floor(Math.random() * (dragon.strength - minDmg + 1) + minDmg);
+}
+
+console.log(dragonDmg(dragon))
