646531f375753b70dd19e24915ce612385f070cc
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-16 14:50:24 -0300

    exercicio 2 completo

--- Fundamentos/bloco8/dia1/script.js
+++ Fundamentos/bloco8/dia1/script.js
@@ -1,17 +1,30 @@
 // Exercício 1
 const newEmployees = (employeesGenerator) => {
-    const employees = {
-      id1: employeesGenerator('Pedro Guerra'),
-      id2: employeesGenerator('Luiza Drumond'), 
-      id3: employeesGenerator('Carla Paiva') 
-    };
-    return employees;
+  const employees = {
+    id1: employeesGenerator("Pedro Guerra"),
+    id2: employeesGenerator("Luiza Drumond"),
+    id3: employeesGenerator("Carla Paiva"),
   };
-  
-  const employeesGenerator = (name) => {
-    const email = name.replace(' ', '_').toLowerCase();
-    return {name, email: `${email}@betrybe.com`};
-  };
-  
-  console.log(newEmployees(employeesGenerator));
-  
+  return employees;
+};
+
+const employeesGenerator = (name) => {
+  const email = name.replace(" ", "_").toLowerCase();
+  return { name, email: `${email}@betrybe.com` };
+};
+
+console.log(newEmployees(employeesGenerator));
+
+// Exercício 2
+
+const sorteio = (playerNumber, verify) => {
+  const numSorteado = Math.floor((Math.random() * 5) + 1);
+
+  return verify(playerNumber, numSorteado) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+const verifyNumber = (playerNumber, sorteadoNumber) => playerNumber === sorteadoNumber;
+
+console.log(sorteio(3, verifyNumber));
+
+// Exercício 3
