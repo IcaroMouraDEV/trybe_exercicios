2e0d0ae46b9177a3537089f33c0bbf0943517c95
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-19 15:41:48 -0300

    exercício 2 e 3 completo

--- Fundamentos/bloco8/dia5/script.js
+++ Fundamentos/bloco8/dia5/script.js
@@ -8,5 +8,31 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
   rectangleArea(...rectangle);
-  console.log(rectangle[0] * rectangle[1]);
+  /* console.log(rectangle[0] * rectangle[1]); */
 });
+
+// Exercício 2
+const sum = (...numbers) => numbers.reduce((accumulator, element) => accumulator + element, 0);
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+// Exercício 3
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes } = person) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+/* console.log(personLikes(alex));
+console.log(personLikes(gunnar)); */
