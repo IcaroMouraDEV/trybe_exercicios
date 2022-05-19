5787f7097fe15935f75fd293fb3e41abbf0fbc2e
Icaro Joel <icaromoura.programador@gmail.com>
2022-05-19 15:53:21 -0300

    Exercício 4

--- Fundamentos/bloco8/dia5/script.js
+++ Fundamentos/bloco8/dia5/script.js
@@ -34,5 +34,35 @@ const gunnar = {
 // complete a assinatura da função abaixo
 const personLikes = ({ name, age, likes } = person) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-/* console.log(personLikes(alex));
-console.log(personLikes(gunnar)); */
+// Exercício 4
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (person) =>  person.filter(({ bornIn, nationality } = element) => bornIn < 2000 && bornIn >= 1900 && nationality === 'Australian');
+
